5c07013105dcec4f80b522a4146ce15ca3dd0e8d
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2023-03-01 23:17:35 +0100

    Include last value on split & loop till vec size

--- quicksort.cpp
+++ quicksort.cpp
@@ -2,19 +2,21 @@
 #include <string>
 #include <vector>
 
+// Splits string with space seperated numbers
+// Doesn't work with multiple spaces in between numbers or space in beginning (i.e. application specific)
 std::vector<int> split(const std::string& string) {
     std::vector<int> values;
     values.reserve((string.length() + 1) / 2);
 
     std::string buffer;
     for (int i = 0; i < string.length(); i++) {
-        bool inside_number = true;
-        if (string[i] == ' ') {
+        if (string[i] == ' ' || i == string.length() - 1) {
+            if (i == string.length() - 1)
+                buffer.push_back(string[i]);
             values.push_back(stoi(buffer));
             buffer.clear();
-            inside_number = false;
         }
-        else if (inside_number)
+        else
             buffer.push_back(string[i]);
     }
     return values;
@@ -22,11 +24,15 @@ std::vector<int> split(const std::string& string) {
 
 int main() {
     std::string input;
-    std::cin >> input;
+    std::getline(std::cin, input);
 
     std::vector<int> values = split(input);
 
-    for (int i = 1; i < values[0]; i++) {
+    // Sort list of numbers
+    
+
+    // Print sorted list
+    for (int i = 1; i < values.size(); i++) {
         std::cout << values[i] << std::endl;
     }
 
